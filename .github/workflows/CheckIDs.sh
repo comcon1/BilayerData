3aec50055db52f05099200d5ea74cb32b73b847b
korbinib <korbinian.bosl@uib.no>
2026-09-17 11:11:52 +0200

    workflows: add ExpSimMetadata autocomplete, rename MolMetadata, update CheckIDs.sh

--- .github/workflows/CheckIDs.sh
+++ .github/workflows/CheckIDs.sh
@@ -118,7 +118,7 @@ else
    list=(${fixlist[${u}]})
    if [[ ${u} -eq 0 ]]; then
     echo "#### Fixing missing indices..." >> $GITHUB_STEP_SUMMARY
-    ordered=($( for path in ${list[@]}; do echo ${path} $( date -d $( grep "DATEOFRUNNING:" ${path}/README.yaml | cut -d" " -f2 | awk -F' |/' '{printf "%s-%s-%s %s",$3,$2,$1,$4}' ) +%s ) $( git log -1 --pretty="format:%ct" "${path}/README.yaml" ); done | sort -n -k2,3 | cut -d" " -f1 ))
+    ordered=($( for path in ${list[@]}; do echo ${path} $( date -d $( grep "DATEOFRUNNING:" ${path}/README.yaml | cut -d" " -f2 | tr -d "'\"" | awk -F'/' '{ if (NF==3) printf "%s-%s-%s",$3,$2,$1; else printf "%s",$1 }' ) +%s ) $( git log -1 --pretty="format:%ct" "${path}/README.yaml" ); done | sort -n -k2,3 | cut -d" " -f1 ))
     for path in ${ordered[@]}; do
      echo "#### ID "${new_id[counter]}" :arrow_right: \`"${path}"\`" >> $GITHUB_STEP_SUMMARY
      echo "ID: "${new_id[counter]} >> ${path}/README.yaml
@@ -126,7 +126,7 @@ else
     done
    else
     echo "#### Fixing duplicated index ${u}..." >> $GITHUB_STEP_SUMMARY
-    ordered=($( for path in ${list[@]}; do echo ${path} $( date -d $( grep "DATEOFRUNNING:" ${path}/README.yaml | cut -d" " -f2 | awk -F' |/' '{printf "%s-%s-%s %s",$3,$2,$1,$4}' ) +%s ) $( git log -1 --pretty="format:%ct" "${path}/README.yaml" ); done | sort -n -k2,3 | cut -d" " -f1 ))
+    ordered=($( for path in ${list[@]}; do echo ${path} $( date -d $( grep "DATEOFRUNNING:" ${path}/README.yaml | cut -d" " -f2 | tr -d "'\"" | awk -F'/' '{ if (NF==3) printf "%s-%s-%s",$3,$2,$1; else printf "%s",$1 }' ) +%s ) $( git log -1 --pretty="format:%ct" "${path}/README.yaml" ); done | sort -n -k2,3 | cut -d" " -f1 ))
     for k in $(seq 1 $((${#ordered[@]}-1))); do
      echo "#### ID "${new_id[counter]}" :arrow_right: \`"${ordered[k]}"\`" >> $GITHUB_STEP_SUMMARY
      sed -i"" "s/ID:.*/ID: ${new_id[counter]}/" ${ordered[k]}/README.yaml
